57b331edf960a7a1a1ed004e472d277833e0700d
Lance <lanc3.corcega@gmail.com>
2023-10-12 16:44:58 +1300

    Section 24.411(Debugging and Unit Testing: Introduction to Unit Testing with JUnit): introduction to JUnit, how to add a Run configuration for unit tests, filling in JUnit stubs with placeholders,

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,33 @@
 public class BankAccount {
+
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    //Branch = true if performing transaction @branchW/teller
+    //False = via ATM
+    public double deposit(double amount, boolean branch) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount, boolean branch) {
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    //~More methods e.g. firstName, lastName, other functions...
+
+
+
 }
