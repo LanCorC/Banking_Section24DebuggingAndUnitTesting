537242dc9a3915ae18f205babbbe6981c956b8ff
Lance <lanc3.corcega@gmail.com>
2023-10-13 14:20:26 +1300

    Section 24.412(Debugging and Unit Testing: Asserts in Junit): introduction to asserts, how to implement testMethods in a testClass, best practices, and test features

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -6,7 +6,9 @@ class BankAccountTest {
 
     @Test
     void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.0, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @Test
@@ -15,7 +17,31 @@ class BankAccountTest {
     }
 
     @Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.0, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+
     }
+
+    @Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.0, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+
+    }
+
+    @Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.0, BankAccount.CHECKING);
+            assertTrue(account.isChecking());
+    }
+
+    @Test
+    public void isChecking_false() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.0, BankAccount.SAVINGS);
+        assertFalse(account.isChecking());
+    }
+
 }
